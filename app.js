fc6c173cf05dbd5af59c06812781dc12230729dd
Ruslan <Alikberov@users.noreply.github.com>
2018-12-09 02:55:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -228,7 +228,7 @@ const server = http.createServer((req, res) => {
 	var	picture = unescape(req.url).match(/nick="(.*?)"&post=(\d)(?:&piece=(\d))/);
 	var	click	= unescape(req.url).match(/\/(\d)(\d)/);
 	var	choice	= unescape(req.url).match(/\/(\d)/);
-	var	chat	= unescape(req.url).match(/chat(?:=(.*)/);
+	var	chat	= unescape(req.url).match(/chat(?:=(.*))/);
 	console.log(req.url);
 	if(picture) {
 		console.log("hXML.open::get?nick::" + picture[1] + "//" + picture[2] + " // " + picture[3]);
