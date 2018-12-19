a1eae5f0e172cda0e8d35ba30261d3f09313ab6f
Ruslan <Alikberov@users.noreply.github.com>
2018-12-20 01:57:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1150,7 +1150,7 @@ const server = http.createServer((req, res) => {
 			res.setHeader("Content-Type", "text/html; charset=utf-8");
 			res.end(szChatBody.
 				  replace(/\(\\ChatNotice\)/gm, chatNotes.join("\r\n")).
-				  replace(/\(\\ChatLogs\)/gm, tmp.join("\r\n")
+				  replace(/\(\\ChatLogs\)/gm, tmp.join("\r\n"))
 			);
 			try { bashMap(ansi); } catch(e) { console.log(e); }
 		}
